913feef97ba074ed3edf677561f88bc39f8f4ba5
Subash Penneru <subashpenneru@gmail.com>
2021-08-28 17:21:33 +0530

    contact page

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,9 +5,6 @@ var menuNav = document.querySelector('.menu-nav');
 var menuBrand = document.querySelector('.menu-branding');
 var navItems = document.querySelectorAll('.nav-item');
 
-// Set the initial state of menu
-var showMenu = false;
-
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
@@ -16,24 +13,4 @@ function toggleMenu() {
   menuNav.classList.toggle('show');
   menuBrand.classList.toggle('show');
   navItems.forEach((item) => item.classList.toggle('show'));
-
-  showMenu = !showMenu;
-
-  //   if (!showMenu) {
-  //     menuBtn.classList.add('close');
-  //     menu.classList.add('show');
-  //     menuNav.classList.add('show');
-  //     menuBrand.classList.add('show');
-  //     navItems.forEach((item) => item.classList.add('show'));
-
-  //     showMenu = true;
-  //   } else {
-  //     menuBtn.classList.remove('close');
-  //     menu.classList.remove('show');
-  //     menuNav.classList.remove('show');
-  //     menuBrand.classList.remove('show');
-  //     navItems.forEach((item) => item.classList.remove('show'));
-
-  //     showMenu = false;
-  //   }
 }
